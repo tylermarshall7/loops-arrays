fe9190dc422973553da7b69562fe27718a559005
Tyler Marshall <66709954+tylermarshall7@users.noreply.github.com>
2020-06-23 13:14:55 -0600

    Initial commit

---
+++ app/loops.js
@@ -0,0 +1,34 @@
+// 1. Within the factorials function, write a loop that begins at 1 and multiplies every consecutive
+// number up to and including the given number(num). Return the total after the loop completes.
+// Example:
+// input: 4
+// output: 24
+// 1*2*3*4 = 24
+
+function factorials(num) {
+
+}
+
+// ------------------------------------------
+
+// 2. Write a function that sums up all of the numbers in a range
+// Example:
+// input: 2, 6
+// output: 20
+
+function rangeTotal(n1, n2) {
+
+}
+
+// ------------------------------------------
+
+
+// 3. Write a function that will take in a starting number, ending number, and a divisor. Iterate
+// over the range of the numbers and rtuen how many numbers can be divided by the divisor.
+// Example:
+// input: 15, 20, 5
+// output: 2
+
+function rangeDivisor(start, end, divisor) {
+
+}
